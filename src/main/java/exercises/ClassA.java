e33d2284539266918cb3af560406a2783111e7df
894921757 <13007505168@163.com>
2020-02-22 18:39:07 +0800

    codeExercises

---
+++ src/main/java/exercises/ClassA.java
@@ -0,0 +1,25 @@
+package exercises;
+
+public class ClassA {
+    static Test1 T = new Test1();
+    static {
+        System.out.println("A");
+    }
+
+    public ClassA() {
+        System.out.println("C");
+    }
+}
+class  Test1 extends  ClassA{
+    static{
+        System.out.println("B");
+    }
+
+    public Test1() {
+        System.out.println("D");
+    }
+
+    public static void main(String[] args) {
+        new Test1();
+    }
+}
