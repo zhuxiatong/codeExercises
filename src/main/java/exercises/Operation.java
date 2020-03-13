baf84d5f33432558084128d3d5cef00ae4b317ff
894921757 <40481487+894921757@users.noreply.github.com>
2020-03-13 11:43:49 +0800

    codeExercises

--- src/main/java/exercises/Operation.java
+++ src/main/java/exercises/Operation.java
@@ -1,6 +1,7 @@
 package exercises;
 
 public enum Operation {
+
     PLUS,MINUS,TIMES,DIVIDE;
     double eval(double x,double y){
         switch(this){
@@ -17,13 +18,23 @@ public enum Operation {
     }
 
     public static void main(String[] args) {
-        System.out.println(Operation.PLUS.eval(3,6));
-        System.out.println(Season.SUMMER.ordinal());
-        for (Season value : Season.values()) {
-            System.out.println(value);
-        }
+//        System.out.println(Operation.PLUS.eval(3,6));
+//        System.out.println(Season.SUMMER.ordinal());
+//        for (Season value : Season.values()) {
+//            System.out.println(value);
+//        }
+        System.out.println(Simpel.zxt.zxt("ll"));
+        System.out.println(Simpel.zxt.zz);
     }
 }
 enum Season{
     SPRING,SUMMER,AUTUMN,WINTER;
+}
+enum Simpel{
+    zxt;
+    public String zz;
+    public Enum zxt(String zz){
+        this.zz=zz;
+       return zxt;
+    }
 }
